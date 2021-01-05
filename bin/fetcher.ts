c6d2b26d1678c75863c56ec677792d7fca802072
zaur <zaur.nadiraliev.228@gmail.com>
2021-01-06 02:31:06 +0300

    Implemented fetching and saving documents

--- bin/fetcher.ts
+++ bin/fetcher.ts
@@ -1,12 +1,14 @@
 #!/usr/bin/env ts-node-script
 
 import { Company, companyUrls } from "../modules/core/company";
-import { createLogger, defaultDirs } from "../modules/core/functions";
+import { createLogger, defaultDirs, shellExec } from "../modules/core/functions";
 import { Fetcher } from "../modules/core/types";
 import { YandexFetcher } from "../modules/fetchers/yndx";
 import { lockers, withLock } from "../modules/core/locker";
 import { config as setupDotenv } from 'dotenv';
 import * as minimist from "minimist";
+import { createConnection } from "typeorm";
+import { ReportEntity } from "../modules/domain/report.entity";
 
 setupDotenv();
 
@@ -21,17 +23,40 @@ fetchers.set('Yndx', new YandexFetcher(createLogger(logDir ? `${logDir}/main` : 
 
 
 withLock(lockers.usingTempDir('fetchers'), async () => {
-  for (const [company, fetcher] of fetchers.entries()) {
-    try {
-      const url = companyUrls[company];
+	const connection = await createConnection('default');
+	const reports = await connection.getRepository(ReportEntity);
 
-      const documents = await fetcher.fetchAllDocuments(url);
+	for (const [company, fetcher] of fetchers.entries()) {
+		try {
+			const url = companyUrls[company];
 
-      console.log(documents);
+			const documents = await fetcher.fetchAllDocuments(url);
 
-    } catch (e) {
-      // only unhandled exception may occur here
-      mainLogger.error(String(e));
-    }
-  }
+			for (const { title, url } of documents) {
+				const found = await reports.findOne({ company, title });
+
+				if (found) {
+					mainLogger.debug(`Skipping ${title}`);
+					continue;
+				}
+
+				const { id } = await reports.save({ title, company });
+				mainLogger.info(`Saved ${title} (${url})`);
+
+				const tempFilepath = documentsDir ? `${documentsDir}/temp.pdf` : `${defaultDirs.fetcherFiles}/temp.pdf`;
+				const reportFilepath = documentsDir ? `${documentsDir}/${id}` : `${defaultDirs.fetcherFiles}/${id}`;
+
+				// todo: this should be implemented via js
+				await shellExec(`
+					curl '${url}' -o ${tempFilepath} 
+					&& pdftotext -layout ${tempFilepath} ${reportFilepath} 
+					&& rm ${tempFilepath}`
+				);
+			}
+
+		} catch (e) {
+			// only unhandled exception may occur here
+			mainLogger.error(String(e));
+		}
+	}
 }).catch(console.error);
